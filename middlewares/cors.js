bcee0edf469a6dcc634d3eb03f4507fd3e8f5729
Root <Dzirrt01@yandex.ru>
2022-02-02 19:52:33 +0300

    55

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -20,7 +20,7 @@ module.exports.cors = (req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   // Если это предварительный запрос, добавляем нужные заголовки
