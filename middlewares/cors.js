883853e0907df7ef90a8e358ebe8c309f1cd7c14
Root <Dzirrt01@yandex.ru>
2022-02-02 19:51:27 +0300

    5

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,12 +9,6 @@ module.exports.cors = (req, res, next) => {
   ];
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
 
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', '*');
-  }
-
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
@@ -22,6 +16,12 @@ module.exports.cors = (req, res, next) => {
 
   // сохраняем список заголовков исходного запроса
   const requestHeaders = req.headers['access-control-request-headers'];
+
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
