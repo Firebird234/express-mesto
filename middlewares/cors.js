ef9b69e55104c850ece9ff46e97af00e6a3d3989
Root <Dzirrt01@yandex.ru>
2022-02-04 19:45:39 +0300

    555

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,37 +1,67 @@
+// module.exports.cors = (req, res, next) => {
+//   const allowedCors = [
+//     'https://best-site.ever.nomoredomains.work',
+//     'http://best-site.ever.nomoredomains.work',
+//     'https://localhost:3000',
+//     'http://localhost:3000',
+//     'http://localhost:3001',
+//     'https://localhost:3001',
+//   ];
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+//   // сохраняем список заголовков исходного запроса
+//   const requestHeaders = req.headers['access-control-request-headers'];
+
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+//     res.header('Access-Control-Allow-Origin', '*');
+//   }
+
+//   // Если это предварительный запрос, добавляем нужные заголовки
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+
+//     // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
+
+//   return next();
+// };
+
+const allowedCors = [
+  'https://general4056-mesto.nomoredomains.rocks',
+  'http://general4056-mesto.nomoredomains.rocks',
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+];
+
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 module.exports.cors = (req, res, next) => {
-  const allowedCors = [
-    'https://best-site.ever.nomoredomains.work',
-    'http://best-site.ever.nomoredomains.work',
-    'https://localhost:3000',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'https://localhost:3001',
-  ];
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  // сохраняем список заголовков исходного запроса
+  const { origin } = req.headers;
+  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', '*');
   }
 
-  // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-
-    // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
 
